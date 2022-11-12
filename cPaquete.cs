b796f5423baaa4c06c7c5645d9369b66d6948e28
AlmadaJuan <almadajuan88@gmail.com>
2022-11-12 10:34:09 -0300

    clases

---
+++ cPaquete.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp_final
+{
+    internal class cPaquete
+    {
+        protected float volumen;
+        protected string tipo;
+        protected float peso;
+        protected int prioridad;
+        protected float beneficio;
+        protected bool apilable;
+        protected bool necesita_elevador;
+              
+    }
+}
